c3d252061c3b9a105a65eaa41ca375d613f80646
Souradji <souradjimohamadou@gmail.com>
2026-08-03 23:14:34 +0100

    Create creer_superadmin.py

---
+++ creer_superadmin.py
@@ -0,0 +1,19 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from apps.users.models import Utilisateur
+
+if not Utilisateur.objects.filter(identifiant='superadmin').exists():
+    Utilisateur.objects.create_superuser(
+        identifiant='superadmin',
+        password='Admin1234!',
+        nom='Admin',
+        prenom='Super',
+        profil='ADMIN',
+    )
+    print('Superadmin créé.')
+else:
+    print('Superadmin existe déjà.')
